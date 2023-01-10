335bd2af863e1cee2d596aa1ef688c27b028bd52
kingseungwooya <ksw_0505@nate.com>
2023-01-10 21:59:06 +0900

    feat: make cell and random generator

--- home.js
+++ home.js
@@ -1,5 +1,46 @@
+const defaultWidth = 1;
+const defaultHeight = 1;
+
+const colorConfig = {
+    red : [0.0, 0.2],
+    orange :[0.2, 0.4],
+    yellow : [0.4, 0.6],
+    green : [0.6, 0.8],
+    blue : [0.8, 1.0]
+}
+
 var runBt = document.getElementById("executeBt");
 var randomArray =  
 runBt.addEventListener("click", function() {
+    var xn = document.getElementById("xn");
+    var yn = document.getElementById("yn");
 
+    let dividedWidth = defaultHeight / xn;
+    let dividedHeight = defaultHeight / yn;
+
+    let cell = new Cell( randomGenerator(),dividedWidth, dividedHeight);
 })
+
+// 0 ~ 1 사잇값 
+function randomGenerator() {
+    return Math.random();
+} 
+function getColorByRandomNumber(weight) {
+    for(var [key, value] in colorConfig) {
+        var min = value[0];
+        var max = value[1];
+        if ( min<= weight < max) {
+            return key;
+        }
+    }
+    
+}
+
+class Cell {
+    constructor ( weight ,width, height ) {
+        this.weight = weight;
+        this.width = width;
+        this.height = height;
+        this.color = getColorByRandomNumber(weight);
+    }
+}
