acec680fa9d55dadbd9b4639515a2235a890e1d5
kingseungwooya <ksw_0505@nate.com>
2023-01-13 02:24:28 +0900

    refactor: refactor code formatte

--- home.js
+++ home.js
@@ -1,14 +1,6 @@
 const defaultWidth = 500;
 const defaultHeight = 500;
 
-const colorConfig = {
-    'red' : [0.0, 0.2],
-    'orange' :[0.2, 0.4],
-    'yellow' : [0.4, 0.6],
-    'green' : [0.6, 0.8],
-    'blue' : [0.8, 1.0]
-}
-
 var gridXY;
 
 var runBt = document.getElementById("executeBt");
@@ -17,68 +9,56 @@ runBt.addEventListener("click", function() {
     var xn = document.getElementById("xn").value;
     var yn = document.getElementById("yn").value;
 
+    // 분할된 각 cell의 width와 height
     let dividedWidth = defaultHeight / xn;
     let dividedHeight = defaultHeight / yn;
-
-
 
     // create nx ny array 
     gridXY = Array(xn).fill().map(() => Array(yn));
 
+    // grid property 설정
     document.documentElement.style.setProperty("--columns", xn);
     document.documentElement.style.setProperty("--width", dividedWidth + "px");
     document.documentElement.style.setProperty("--rows", yn);
     document.documentElement.style.setProperty("--height", dividedHeight + "px");
 
 
-    var gridPrefix = "repeat(";
-    var gridPostfix = ", 1fr);" 
+    // grid 초기화
     var grid = document.getElementById("gridBox");
-    
- 
-    grid.style.gridTemplateColumns = gridPrefix + xn + gridPostfix;
-    grid.style.gridTemplateRows = gridPrefix + yn + gridPostfix;
-        
-
     grid.innerHTML = "";
     
     for ( var i = 0; i < yn; i++ ) {
         gridXY[i] = new Array();
         for ( var j = 0; j < xn; j++ ) {
-            //var itemOfGrid = gridXY[i][j];
 
             let cell = new Cell( randomGenerator(),dividedWidth, dividedHeight);
             gridXY[i][j] = cell;
             var item = document.createElement("div");
             item.className = 'item';
-
             item.style.backgroundColor = cell['color'];
-            
-            
-            var text = document.createTextNode(cell['weight']);
-            //item.appendChild(text);
             
             grid.appendChild(item);
         }
     }
-    
-     console.log(gridXY);
+
 })
 
-// 0 ~ 1 사잇값 
+// 0 ~ 1 사잇값 소수점 5자리까지 생성
 function randomGenerator() {
     return Math.random().toFixed(5);
 } 
 
-
+// 가중치를 통해 red blue 값 조정
 function getRGBByWeight(weight) {
-    let blueWeight = (255 * ( 1 - weight )).toFixed(5);
-    let redWeight = (255 * weight).toFixed(5);
-    let rgb = `rgb(${redWeight}, 0, ${blueWeight})`;
-    console.log(rgb);
+
+    let red = (255 * weight).toFixed(5);
+    let blue = (255 * ( 1 - weight )).toFixed(5);
+
+    let rgb = `rgb(${red}, 0, ${blue})`;
     return rgb;
 }
 
+// cell class
 class Cell {
     constructor ( weight ,width, height ) {
         this.weight = weight;
