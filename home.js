dd7f07ce30115a86dc7ca65390fa0a2612bba55d
kingseungwooya <ksw_0505@nate.com>
2023-01-29 16:12:57 +0900

    feat: RGB 함수 구현

--- home.js
+++ home.js
@@ -4,6 +4,10 @@ const defaultHeight = 500;
 
 var gridXY;
 
+var minOfOmega;
+var maxOfOmega;
+
+
 var runBt = document.getElementById("executeBt");
 var randomArray = runBt.addEventListener("click", function () {
   var xn = document.getElementById("xn").value;
@@ -12,9 +16,6 @@ var randomArray = runBt.addEventListener("click", function () {
   let dividedWidth = defaultHeight / xn;
   let dividedHeight = defaultHeight / yn;
 
-  //gridXY[j+1][i] - 4*gridXY[j][i] + gridXY[j-1][i] + gridXY[j][i+1] + gridXY[j][i-1] = 0;
-  //gridXY[j][i] =
-  // create nx ny array
   gridXY = Array(xn)
     .fill()
     .map(() => Array(yn));
@@ -32,7 +33,7 @@ var randomArray = runBt.addEventListener("click", function () {
   grid.style.gridTemplateRows = gridPrefix + yn + gridPostfix;
 
   grid.innerHTML = "";
-  // 값의 배열 초기화
+  // 값의 배열(b) 초기화
   let valueCount = xn * yn;
   let valueArray = new Array(valueCount).fill(0);
 
@@ -45,8 +46,7 @@ var randomArray = runBt.addEventListener("click", function () {
     for (var j = 1; j <= xn; j++) {
       // 계수행렬 구하기
       coefficientMatrix[rowCount] = new Array();
-
-      coefficientMatrix[rowCount] = getCoefficientMatrix(j, i, xn, yn);
+      coefficientMatrix[rowCount] = getCoefficientArray(j, i, xn, yn);
 
       // 값 배열 구하기
       if (j == Math.ceil(yn / 2) && i == Math.ceil(xn / 2)) {
@@ -54,19 +54,6 @@ var randomArray = runBt.addEventListener("click", function () {
         valueArray[rowCount] = 100; // 중간값은 100으로 value 생성
       }
       rowCount++;
-
-      /*
-            var omega = getOmegaByWeight() 
-            let cell = new Cell( weight ,dividedWidth, dividedHeight, omega);
-            gridXY[i][j] = cell;
-            var item = document.createElement("div");
-            item.className = 'item';
-           
-            item.style.backgroundColor = cell['color'];
-            var text = document.createTextNode(cell['weight']);
-            //item.appendChild(text);
-            
-            grid.appendChild(item);*/
     }
   }
 
@@ -74,16 +61,31 @@ var randomArray = runBt.addEventListener("click", function () {
   console.log(coefficientMatrix);
 
   // solve x by 계수행렬 and 값 배열
-  let result = solveSystemOfLinearEquations(coefficientMatrix, valueArray);
   // let resultByCramer = cramer(valueCount,coefficientMatrix, valueArray)
+  let result = gaussianElimination(coefficientMatrix, valueArray);
   console.log(result);
+  minOfOmega = Math.min(result);
+  maxOfOmega = Math.max(result);
+
+
+  let count = 0;
+  for( row = 0; row < xn; row++) {
+    for( col = 0; col < yn; col++) {
+      var omega = result[count];
+      var color = getColorByOmega(omega, minOfOmega, maxOfOmega);
+      //let cell = new Cell( weight ,dividedWidth, dividedHeight, omega);
+      //gridXY[i][j] = cell;
+      var item = document.createElement("div");
+      item.className = 'item';
+           
+      item.style.backgroundColor =color;
+      grid.appendChild(item);
+      count++;
+    }
+  }
+
 });
 
-// 0 ~ 1 사잇값
-function randomGenerator() {
-  return Math.random().toFixed(1);
-}
-function getOmegaByWeight(weight) {}
 
 function getColorByRandomNumber(weight) {
   for (key in colorConfig) {
@@ -97,6 +99,21 @@ function getColorByRandomNumber(weight) {
     }
   }
 }
+function getColorByOmega(omega, min, max) {
+  if( omega < 0) {
+    var red = (min - value) * (255/(max-min));
+    var blue = 0;
+    var green = (value - max) * (255/(max-min));
+  } else {
+    var blue = (max - value) * (255/(max-min));
+    var red = 0;
+    var green = (value - min) * (255/(max-min));
+  }
+  
+
+  let rgb = `rgb(${red}, ${green}, ${blue}})`;
+  return rgb;
+}
 
 class Cell {
   constructor(weight, width, height, omega) {
@@ -104,20 +121,44 @@ class Cell {
     this.width = width;
     this.height = height;
     this.omega = omega;
-    this.color = getColorBy(omega);
+    this.color = getColorByOmega(omega);
   }
-  set weight(value) {
-    this.weight = 100.0;
-  }
+  
 }
+// input 값으로는 구하려는 파이의 좌표가 입력된다  파이(n , m)의 ( nxm 개의 )선형방정식
+function getCoefficientArray(piI, piJ, nx, ny) {
+    //gridXY[j+1][i] - 4*gridXY[j][i] + gridXY[j-1][i] + gridXY[j][i+1] + gridXY[j][i-1] = 0;
+    const FOUR = Number(-4);
+    const ONE = Number(1);
+    const ZERO = Number(0);
+
+    let row = new Array();
+    for (let j = 1; j <= ny; j++) {
+      for (let i = 1; i <= nx; i++) {
+        if (i == piI && j == piJ) {
+          row.push(FOUR);
+        } else if (
+          (i == piI - 1 && j == piJ) ||
+          (i == piI && j == piJ - 1) ||
+          (i == piI + 1 && j == piJ) ||
+          (i == piI && j == piJ + 1)
+        ) {
+          row.push(ONE);
+        } else {
+          row.push(ZERO);
+        }
+      }
+      
+    }
+    return row;
+  }
 // 가우스 소거법
 function solveSystemOfLinearEquations(A, b) {
-  // Check for compatibility
+  // Validation 
   if (A.length !== b.length) {
     return "Error: incompatible systems";
   }
 
-  // Solve for coefficients
   let x = [];
   for (let i = 0; i < A.length; i++) {
     let sum = 0;
@@ -135,8 +176,67 @@ function solveSystemOfLinearEquations(A, b) {
     }
   }
 
-  // Return solution
   return x;
+}
+function gaussianElimination(A, b) {
+    var n = A.length;
+
+    for (var i = 0; i < n; i++) {
+        // Search for maximum in this column
+        var maxEl = Math.abs(A[i][i]),
+            maxRow = i;
+        for (var k = i + 1; k < n; k++) {
+            if (Math.abs(A[k][i]) > maxEl) {
+                maxEl = Math.abs(A[k][i]);
+                maxRow = k;
+            }
+        }
+        //If the max element is zero, then the system has no unique solution
+        if(maxEl == 0){
+            return 'System has no unique solution';
+        }
+
+        // Swap maximum row with current row (column by column)
+        for (var k = i; k < n + 1; k++) {
+            var tmp = A[maxRow][k];
+            A[maxRow][k] = A[i][k];
+            A[i][k] = tmp;
+            if(k == n){
+                var tmpB = b[maxRow];
+                b[maxRow] = b[i];
+                b[i] = tmpB;
+            }
+        }
+
+        // Make all rows below this one 0 in current column
+        for (k = i + 1; k < n; k++) {
+            var c = -A[k][i] / A[i][i];
+            for (var j = i; j < n + 1; j++) {
+                if (i == j) {
+                    A[k][j] = 0;
+                } else {
+                    A[k][j] += c * A[i][j];
+                }
+                if(j == n){
+                    b[k] += c * b[i];
+                }
+            }
+        }
+    }
+
+    // Solve equation Ax=b for an upper triangular matrix A
+    var x = new Array(n);
+    for (var i = n - 1; i > -1; i--) {
+        if(A[i][i] === 0){
+            return 'System has no unique solution';
+        }
+        x[i] = b[i] / A[i][i];
+        for (var k = i - 1; k > -1; k--) {
+            b[k] -= A[k][i] * x[i];
+        }
+    }
+
+    return x;
 }
 
 // 선형방정식의 갯수 n x m  에서 행 m 개 입력  , 값의 배열 b 계수의 배열 a
@@ -201,27 +301,4 @@ function getDeterminant(matrix, n) {
 
   return sum;
 }
-// input 값으로는 구하려는 파이의 좌표가 입력된다  파이(n , m)의 ( nxm 개의 )선형방정식
-function getCoefficientMatrix(piI, piJ, nx, ny) {
-  //gridXY[j+1][i] - 4*gridXY[j][i] + gridXY[j-1][i] + gridXY[j][i+1] + gridXY[j][i-1] = 0;
-  let row = [];
-  for (let j = 1; j <= ny; j++) {
-    for (let i = 1; i <= nx; i++) {
-      if (i == piI && j == piJ) {
-        row.push(-4);
-      } else if (
-        (i == piI - 1 && j == piJ) ||
-        (i == piI && j == piJ - 1) ||
-        (i == piI + 1 && j == piJ) ||
-        (i == piI && j == piJ + 1)
-      ) {
-        row.push(1);
-      } else {
-        row.push(0);
-      }
-    }
-  }
-  return row;
-}
 
-function getValueArray(length) {}
