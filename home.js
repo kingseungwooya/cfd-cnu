2d3c6f4002fc8c23a4c8cb1622fa275984d3f2ad
kingseungwooya <ksw_0505@nate.com>
2023-03-01 17:37:15 +0900

    feat: 속도 성분 u, v를 찾고 가시화한다.

--- home.js
+++ home.js
@@ -56,6 +56,9 @@ runBt.addEventListener("click", function () {
   let result = laplacianSOR(xn, yn,epsilon, omega, b, maxIter);
   let endTime = new Date();
   console.log(endTime - startTime);
+  
+  findUV(result, dividedWidth, dividedHeight);
+ 
   // 결과 내 최대 최소 값 구하기 
 
   maxOfPis = result.reduce((acc, curr) => Math.max(acc, ...curr), Number.NEGATIVE_INFINITY);
@@ -77,4 +80,121 @@ runBt.addEventListener("click", function () {
   }
 });
 
+function findUV(p, dx, dy) {
+  const n = p.length;
+  const m = p[0].length;
+  const u = new Array(n);
+  const v = new Array(n);
+
+  for (let i = 0; i < n; i++) {
+    u[i] = new Array(m).fill(0);
+  }
+
+  for (let i = 0; i < n; i++) {
+    v[i] = new Array(m).fill(0);
+  }
+  console.log("u is" );
+  console.log( u);
+
+  for (let i = 1; i < n - 1; i++) {
+    for (let j = 1; j < m - 1; j++) {
+      u[i][j] = (p[i+1][j] - p[i-1][j])  / (2 * dx);
+      v[i][j] = (p[i][j+1] - p[i][j-1]) / (2 * dy);
+    }
+  }
+  u[0][0] = 0;
+  u[0][1] = 0;
+  u[1][0] = 0;
+  v[0][0] = 0;
+  v[1][0] = 0;
+  v[0][1] = 0;
+  u[n - 1][m - 1] = 0;
+  v[n - 1][m - 1] = 0;
+  u[n - 1][m - 2] = 0;
+  v[n - 1][m - 2] = 0;
+  u[n - 2][m - 1] = 0;
+  v[n - 2][m - 1] = 0;
+
+  drawArrows(n, m, dx, dy, u, v);
+  return { u, v };
+
+
+
+}
+function drawArrows(n, m, dx, dy, u, v) {
+  d3.selectAll(".svg").select("svg").remove();
+  document.querySelectorAll(".svg").innerHTML = "";
+  const svg = d3.select("body")
+    .append("svg")
+    .attr("class", "svg")
+    .attr("width", 500)
+    .attr("height", 500);
+    
+  const CELL_SIZE = Math.min(svg.attr("width") / m, svg.attr("height") / n);
+  
+  const arrowSize = CELL_SIZE * 0.2;
+  const arrowheads = [
+    "M 0 0 L -5 -10 L 5 -10 z",
+    "M 0 0 L -5 10 L 5 10 z"
+  ];
+  
+  const arrowheadScale = d3.scaleLinear()
+    .domain([-1, 1])
+    .range([-arrowSize / 2, arrowSize / 2]);
+  
+  const lineData = [];
+  for (let i = 0; i < n ; i++) {
+    for (let j = 0; j < m ; j++) {
+      const x = j * CELL_SIZE + CELL_SIZE / 2;
+      const y = (n - 1 - i) * CELL_SIZE + CELL_SIZE / 2;
+      
+      // Calculate the center of the cell
+      const x0 = j * dx + dx / 2;
+      const y0 = i * dy + dy / 2;
+      
+      // Calculate the end point of the line
+      const x1 = x0 + arrowheadScale(u[i][j]);
+      const y1 = y0 - arrowheadScale(v[i][j]);
+
+      lineData.push({x1, y1, x2: x0, y2: y0});
+      
+      const arrowhead = arrowheads[Math.sign(v[i][j]) === 1 ? 0 : 1];
+      /*
+      const arrow = svg.append("path")
+        .attr("d", arrowhead)
+        .attr("transform", `translate(${x},${y}) rotate(${Math.atan2(-v[i][j], u[i][j]) * 180 / Math.PI})`)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
+        .attr("fill", "none");*/
+    }
+  }
+  
+  const lines = svg.selectAll("line")
+    .data(lineData)
+    .enter()
+    .append("line")
+    .attr("x1", d => d.x1)
+    .attr("y1", d => d.y1)
+    .attr("x2", d => d.x2)
+    .attr("y2", d => d.y2)
+    .attr("stroke", "black")
+    .attr("stroke-width", 20)
+    .attr("marker-end", d => `url(#arrow-${d.x2}-${d.y2})`);
+  
+    
+    const arrowMarkers = svg.selectAll("marker")
+    .data(lineData)
+    .enter()
+    .append("marker")
+    .attr("id", d => `arrow-${d.x2}-${d.y2}`)
+    .attr("markerWidth", arrowSize)
+    .attr("markerHeight", arrowSize)
+    .attr("refX", arrowSize / 2)
+    .attr("refY", arrowSize / 2)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+}
 
